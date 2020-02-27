74ba2b92e3c2218be0ef3b5da93cb124080d564d
lhoussaine <howa.hiya.1993@gmail.com>
2020-02-27 13:52:53 +0100

    first commit

---
+++ first.rb
@@ -0,0 +1,14 @@
+module Enumerable
+  def try
+    if block_given?
+      0.upto(length - 1) do |i|
+        yield self[i]
+      end
+    else
+      puts 'no block given'
+    end
+  end
+end
+x = [5, 8, -3, 5, 5, 10]
+
+x.try { |num| puts num * 2 }
